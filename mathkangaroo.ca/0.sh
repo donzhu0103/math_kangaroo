df87b742b94cbc1254baf9d144e862c965f368a7
Don Zhu <donzhu0103@gmail.com>
2023-02-02 18:14:12 -0700

    separate year

--- mathkangaroo.ca/0.sh
+++ mathkangaroo.ca/0.sh
@@ -1,14 +1,19 @@
 
 for i in *.pdf; do
-    id=`echo $i | grep -e gr -e an | cut -c7-10`
+
+    # id=`echo $i | grep -e gr -e an | cut -c1-4`
+    id=`echo $i | cut -c1-4`
     if [ "$id" == '' ]; then 
         continue
     fi
     echo $id
-    # if [ ! -e $id ]; then
-    #     mkdir $id
-    # fi
-    # mv "$i" $id
+
+    if [ ! -e $id ]; then
+        mkdir $id
+    fi
+
+    mv "$i" $id/.
+
 done
 
 echo DONE.
